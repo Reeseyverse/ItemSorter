6685f8d02aa6cffbe3f7f789ed2ea475d666d076
Reeseyverse <jorrisecko@gmail.com>
2024-04-10 15:48:08 -0400

    finished

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -6,10 +6,27 @@ import java.util.Comparator;
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        if (items == null || items.length == 0) {
+            return new Item[0];
+        }
+
+        for (int i = 0; i < items.length -1; i++) {
+            int minNum = i;
+            for (int j = i + 1; j < items.length; j++) {
+                if (comparator.compare(items[j], items[minNum]) < 0) {
+                    minNum = j;
+                }
+            }
+            Item temp = items[minNum];
+            items[minNum] = items[i];
+            items[i] = temp;
+        }
+        return items;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item1, Item item2) {
+        if (item1.getPrice() > item2.getPrice()) {
+            return 1;
+        } else if (Objects.equals(item1.getPrice(), item2.getPrice())) {
+            return 0;
+        }
+        return -1;
+    }
 }
